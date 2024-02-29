3b27d46ea348fe33e3c5a14b7cf2a64fed072087
Dorothy <julianne.attai@gmail.com>
2024-02-29 23:31:16 +0100

    boilerplate code from repo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetson/ROB498_FlightExercises/src
+# Build directory: /home/jetson/ROB498_FlightExercises/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
