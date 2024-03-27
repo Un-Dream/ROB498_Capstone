d4fda8ffea91fb27d92ac1342187e051e75ac375
5716151012 <158003117+5716151012@users.noreply.github.com>
2024-03-27 11:20:44 -0400

    Add files via upload

---
+++ sim commands for FT3.py
@@ -0,0 +1,26 @@
+for flight test 3  
+
+#in terminal 1:
+no_sim=1 make px4_sitl_default gazebo
+
+#in termianl 2:
+cd <PX4-Autopilot_clone>
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+
+#in terminal 3:
+roslaunch gazebo_ros empty_world.launch world_name:=$(pwd)/Tools/sitl_gazebo/worlds/iris.world
+
+
+#to add wind 
+<plugin name='wind_plugin' filename='libgazebo_wind_plugin.so'>
+      <frameId>base_link</frameId>
+      <robotNamespace/>
+      <xyzOffset>1 0 0</xyzOffset>
+      <windDirectionMean>0 1 0</windDirectionMean>
+      <windVelocityMean>SET_YOUR_WIND_SPEED</windVelocityMean>
+      <windGustDirection>0 0 0</windGustDirection>
+      <windGustDuration>0</windGustDuration>
+      <windGustStart>0</windGustStart>
+      <windGustVelocityMean>0</windGustVelocityMean>
+      <windPubTopic>world_wind</windPubTopic>
+    </plugin>
