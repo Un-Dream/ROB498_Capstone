4aa278bcc5fc7b5a03c4abd0b155e23321a5efd3
Un-Dream <morgan.tran.student@gmail.com>
2024-04-09 14:33:03 -0400

    detect code

--- src/sar/camera.py
+++ src/sar/camera.py
@@ -4,11 +4,18 @@ from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
 from gst_cam import camera
+import numpy as np
+from geometry_msgs.msg import PoseStamped, TransformStamped, Pose, PoseArray
+from nav_msgs.msg import Odometry
+
 
 class Camera:
     def __init__(self):
         rospy.init_node('imx219_node', anonymous=True)
         self.publisher = rospy.Publisher('/imx/images', Image, queue_size=10)
+        self.publisher_detection = rospy.Publisher('/detection/pose', Pose, queue_size=10)
+        self.mavros_pose = rospy.Subscriber('/mavros/odometry/in', Odometry, self.mavros_callback)
+
 
         # Initialize the OpenCV bridge
         self.width = 1640
@@ -16,6 +23,9 @@ class Camera:
         self.camera = cv2.VideoCapture(camera(0, self.width, self.height))
         self.rate = rospy.Rate(1)
         self.bridge = CvBridge()
+
+    def mavros_callback(self,msg):
+        self.curr_position = msg.pose.pose.position
 
     def start(self):
             
@@ -35,7 +45,63 @@ class Camera:
                 #This is now publishing our images
                 #Make a new node to sub to /imx/images, and positional stuff 
                 #process using opencv code <<--- AMY
+
+
+                 # color space 
+                hsvFrame = cv2.cvtColor(frame_0, cv2.COLOR_BGR2HSV) 
+            
+                # Set range for red color and  
+                # define mask 
+                red_lower = np.array([136, 87, 111], np.uint8) 
+                red_upper = np.array([180, 255, 255], np.uint8) 
+                red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+                    # Creating contour to track red color 
+                contours, hierarchy = cv2.findContours(red_mask, 
+                                                    cv2.RETR_TREE, 
+                                                    cv2.CHAIN_APPROX_SIMPLE) 
                 
+                colour_list = []
+                
+                for pic, contour in enumerate(contours): 
+                    area = cv2.contourArea(contour) 
+                    if(area > 300): 
+   
+                        #find the center of the contour
+                        M = cv2.moments(contour)
+                        cX = int(M["m10"] / M["m00"])
+                        cY = int(M["m01"] / M["m00"])
+                        # cv2.circle(imageFrame, (cX, cY), 5, (255, 255, 255), -1)
+
+                        #backwards projection
+                        #get the depth of the pixel
+                        z = self.curr_position.z
+                        #get the x and y of the pixel
+                        x = cX
+                        y = cY
+
+                        #camera intrinsics
+                        K = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+
+                        """
+                        K = np.array([[focal_length_x, 0, principal_point_x],
+                        [0, focal_length_y, principal_point_y],
+                        [0, 0, 1]])
+                        """
+                        K_inv = np.linalg.inv(K)
+
+                        #get the pixel in camera frame
+                        pixel = np.array([x, y, 1])
+                        point_camera = np.dot(K_inv, pixel) * z
+
+                        #get the pixel in world frame
+                        point_world = self.curr_position + point_camera
+
+                        #publish the point
+
+                        self.publisher_detection.publish(point_world)
+
+
+                            
 
 
                 # Convert the frame to a ROS Image message
